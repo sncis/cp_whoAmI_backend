f14b16aaed83af29c55b186eb56b6b0eb5a4d598
snzew <Sitan.zewde@code.berlin>
2022-03-22 11:28:28 +0100

    edit cors

--- src/app.js
+++ src/app.js
@@ -14,33 +14,33 @@ app.use(express.json({limit:'2000b',type: 'application/json'}))
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(cors(({
-	origin: ["http://localhost:3000", 'https://cpwhoami.herokuapp.com/'],
+app.use(cors({
+	origin: ["http://localhost:3000", 'https://cpwhoami.herokuapp.com'],
 	credentials: true,
 	exposeHeaders:['Content-Length', 'X-Frame-Options', 'X-XSS-Protection','X-Content-Type-Options']
-})))
+}))
 
-app.use('/ip', routes.ip)
-app.use('/fingerprint',routes.fingerprint)
+
+app.use((req,res,next) => {
+	res.set("Access-Control-Allow-Origin", "http://localhost:3000 https://cpwhoami.herokuapp.com")
+	res.set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+	res.set("Content-Type", 'application/json')
+	res.set('Strict-Transport-Security', 'max-age=15552000')
+	res.set('Content-Security-Policy',	"default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'")
+	res.set("X-Frame-Options", "deny")
+	res.set("X-XSS-Protection", "1;mode=block")
+	res.set("X-Content-Type-Options", "nosniff")
+	res.set("Feature-Policy", "camera 'none'; autoplay 'none'; fullscreen 'none'; geolocation 'none'; gyroscope 'none';  magnetometer 'none'; microphone 'none'; midi 'none'; payment 'none'; picture-in-picture 'none'; publickey-credentials-get 'none'; sync-xhr 'none'; usb 'none'; xr-spatial-tracking 'none'",
+	)
+	next()
+})
+
+app.use('/ip', cors(),routes.ip)
+app.use('/fingerprint', cors(), routes.fingerprint)
 
 app.use(jsonValidationErrorResponder)
 app.use(errorLogger)
 app.use(errorResponder)
-
-app.use((req,res,next) => {
-	// res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
-	res.header("Content-Type", 'application/json')
-
-	// res.header('Strict-Transport-Security', 'max-age=15552000')
-	// res.header('Content-Security-Policy',	"default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'")
-	// res.header("X-Frame-Options", "deny")
-	// res.header("X-XSS-Protection", "1;mode=block")
-	// res.header("X-Content-Type-Options", "nosniff")
-	// res.header("Feature-Policy", "camera 'none'; autoplay 'none'; fullscreen 'self'; geolocation 'self'; gyroscope 'self';  magnetometer 'self'; microphone 'none'; midi 'none'; payment 'none'; picture-in-picture 'none'; publickey-credentials-get 'none'; sync-xhr 'self'; usb 'none'; xr-spatial-tracking 'none'",
-	// )
-	next()
-})
 
 db.connect(error => {
 	if(error){
